8fba2ef5171678e573a3779f568bb5c3f755c3c5
Yuta Takahashi <b1015143@fun.ac.jp>
2019-02-26 14:03:19 +0900

    :shirt: ソースをきれいに
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- ns-3.29/scratch/low-rate-dos-attack.cc
+++ ns-3.29/scratch/low-rate-dos-attack.cc
@@ -1,4 +1,4 @@
-/* 
+/*
  * The topology used to simulate this attack contains 5 nodes as follows:
  * n0 -> alice (sender)
  * n1 -> eve (attacker)
@@ -53,117 +53,111 @@ uint32_t newTotalBytes;
 
 void TraceThroughput (Ptr<Application> app, Ptr<OutputStreamWrapper> stream)
 {
-	Ptr <PacketSink> pktSink = DynamicCast <PacketSink> (app);
-      	newTotalBytes = pktSink->GetTotalRx ();
+    Ptr <PacketSink> pktSink = DynamicCast <PacketSink> (app);
+    newTotalBytes = pktSink->GetTotalRx ();
 	// messure throughput in Kbps
-	//fprintf(stdout,"%10.4f %f\n",Simulator::Now ().GetSeconds (), 
-	//	(newTotalBytes - oldTotalBytes)*8/0.1/1024);
-  	*stream->GetStream() << Simulator::Now ().GetSeconds () 
-		<< " \t " << (newTotalBytes - oldTotalBytes)*8.0/0.1/1024 << std::endl;
+	//fprintf(stdout,"%10.4f %f\n",Simulator::Now ().GetSeconds (), (newTotalBytes - oldTotalBytes)*8/0.1/1024);
+  	*stream->GetStream() << Simulator::Now ().GetSeconds () << " \t " << (newTotalBytes - oldTotalBytes)*8.0/0.1/1024 << std::endl;
 	oldTotalBytes = newTotalBytes;
 	Simulator::Schedule (Seconds (TH_INTERVAL), &TraceThroughput, app, stream);
 }
 
 int main(int argc, char *argv[])
 {
-  CommandLine cmd;
-  cmd.Parse(argc, argv);
+    CommandLine cmd;
+    cmd.Parse(argc, argv);
 
-  Time::SetResolution(Time::NS);
-  LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
-
-
-  NodeContainer nodes;
-  nodes.Create(5);
+    Time::SetResolution(Time::NS);
+    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
 
 
-  // Default TCP is TcpNewReno (no need to change, unless experimenting with other variants)
-  Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::TcpNewReno"));
+    NodeContainer nodes;
+    nodes.Create(5);
 
-  // Define the Point-to-Point links (helpers) and their paramters
-  PointToPointHelper pp1, pp2;
-  pp1.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-  pp1.SetChannelAttribute("Delay", StringValue("1ms"));
 
-  // Add a DropTailQueue to the bottleneck link
+    // Default TCP is TcpNewReno (no need to change, unless experimenting with other variants)
+    Config::SetDefault("ns3::TcpL4Protocol::SocketType", StringValue("ns3::TcpNewReno"));
+
+    // Define the Point-to-Point links (helpers) and their paramters
+    PointToPointHelper pp1, pp2;
+    pp1.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
+    pp1.SetChannelAttribute("Delay", StringValue("1ms"));
+
+    // Add a DropTailQueue to the bottleneck link
     pp2.SetQueue ("ns3::DropTailQueue","MaxSize", StringValue ("50p"));
-  pp2.SetDeviceAttribute("DataRate", StringValue("1.5Mbps"));
-  pp2.SetChannelAttribute("Delay", StringValue("20ms"));
+    pp2.SetDeviceAttribute("DataRate", StringValue("1.5Mbps"));
+    pp2.SetChannelAttribute("Delay", StringValue("20ms"));
 
 
-  // Install the Point-to-Point links between nodes
-  NetDeviceContainer d02, d12, d23, d34;
-  d02 = pp1.Install(nodes.Get(0), nodes.Get(2));
-  d12 = pp1.Install(nodes.Get(1), nodes.Get(2));
-  d23 = pp2.Install(nodes.Get(2), nodes.Get(3));
-  d34 = pp1.Install(nodes.Get(3), nodes.Get(4));
+    // Install the Point-to-Point links between nodes
+    NetDeviceContainer d02, d12, d23, d34;
+    d02 = pp1.Install(nodes.Get(0), nodes.Get(2));
+    d12 = pp1.Install(nodes.Get(1), nodes.Get(2));
+    d23 = pp2.Install(nodes.Get(2), nodes.Get(3));
+    d34 = pp1.Install(nodes.Get(3), nodes.Get(4));
 
 
-  InternetStackHelper stack;
-  stack.Install(nodes);
+    InternetStackHelper stack;
+    stack.Install(nodes);
 
 
-  Ipv4AddressHelper a02, a12, a23, a34;
-  a02.SetBase("10.1.1.0", "255.255.255.0");
-  a12.SetBase("10.1.2.0", "255.255.255.0");
-  a23.SetBase("10.1.3.0", "255.255.255.0");
-  a34.SetBase("10.1.4.0", "255.255.255.0");
+    Ipv4AddressHelper a02, a12, a23, a34;
+    a02.SetBase("10.1.1.0", "255.255.255.0");
+    a12.SetBase("10.1.2.0", "255.255.255.0");
+    a23.SetBase("10.1.3.0", "255.255.255.0");
+    a34.SetBase("10.1.4.0", "255.255.255.0");
 
 
-  Ipv4InterfaceContainer i02, i12, i23, i34;
-  i02 = a02.Assign(d02);
-  i12 = a12.Assign(d12);
-  i23 = a23.Assign(d23);
-  i34 = a34.Assign(d34);
+    Ipv4InterfaceContainer i02, i12, i23, i34;
+    i02 = a02.Assign(d02);
+    i12 = a12.Assign(d12);
+    i23 = a23.Assign(d23);
+    i34 = a34.Assign(d34);
 
 
-  // UDP On-Off Application - Application used by attacker (eve) to create the low-rate bursts.
-  OnOffHelper onoff("ns3::UdpSocketFactory",
-                    Address(InetSocketAddress(i34.GetAddress(1), UDP_SINK_PORT)));
-  onoff.SetConstantRate(DataRate(ATTACKER_RATE));
-  onoff.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=" + ON_TIME + "]"));
-  onoff.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=" + OFF_TIME + "]"));
-  ApplicationContainer onOffApp = onoff.Install(nodes.Get(1));
-  onOffApp.Start(Seconds(ATTACKER_START));
-  onOffApp.Stop(Seconds(MAX_SIMULATION_TIME));
+    // UDP On-Off Application - Application used by attacker (eve) to create the low-rate bursts.
+    OnOffHelper onoff("ns3::UdpSocketFactory", Address(InetSocketAddress(i34.GetAddress(1), UDP_SINK_PORT)));
+    onoff.SetConstantRate(DataRate(ATTACKER_RATE));
+    onoff.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=" + ON_TIME + "]"));
+    onoff.SetAttribute("OffTime", StringValue("ns3::ConstantRandomVariable[Constant=" + OFF_TIME + "]"));
+    ApplicationContainer onOffApp = onoff.Install(nodes.Get(1));
+    onOffApp.Start(Seconds(ATTACKER_START));
+    onOffApp.Stop(Seconds(MAX_SIMULATION_TIME));
 
 
-  // TCP Bulk Send Application - Application used by the legit node (alice) to send data to a receiver.
-  BulkSendHelper bulkSend("ns3::TcpSocketFactory",
-                          InetSocketAddress(i34.GetAddress(1), TCP_SINK_PORT));
-  bulkSend.SetAttribute("MaxBytes", UintegerValue(BULK_SEND_MAX_BYTES));
-  ApplicationContainer bulkSendApp = bulkSend.Install(nodes.Get(0));
-  bulkSendApp.Start(Seconds(SENDER_START));
-  bulkSendApp.Stop(Seconds(MAX_SIMULATION_TIME));
+    // TCP Bulk Send Application - Application used by the legit node (alice) to send data to a receiver.
+    BulkSendHelper bulkSend("ns3::TcpSocketFactory", InetSocketAddress(i34.GetAddress(1), TCP_SINK_PORT));
+    bulkSend.SetAttribute("MaxBytes", UintegerValue(BULK_SEND_MAX_BYTES));
+    ApplicationContainer bulkSendApp = bulkSend.Install(nodes.Get(0));
+    bulkSendApp.Start(Seconds(SENDER_START));
+    bulkSendApp.Stop(Seconds(MAX_SIMULATION_TIME));
 
 
-  // UDP sink on the receiver (bob).
-  PacketSinkHelper UDPsink("ns3::UdpSocketFactory",
-                           Address(InetSocketAddress(Ipv4Address::GetAny(), UDP_SINK_PORT)));
-  ApplicationContainer UDPSinkApp = UDPsink.Install(nodes.Get(4));
-  UDPSinkApp.Start(Seconds(0.0));
-  UDPSinkApp.Stop(Seconds(MAX_SIMULATION_TIME));
+    // UDP sink on the receiver (bob).
+    PacketSinkHelper UDPsink("ns3::UdpSocketFactory", Address(InetSocketAddress(Ipv4Address::GetAny(), UDP_SINK_PORT)));
+    ApplicationContainer UDPSinkApp = UDPsink.Install(nodes.Get(4));
+    UDPSinkApp.Start(Seconds(0.0));
+    UDPSinkApp.Stop(Seconds(MAX_SIMULATION_TIME));
 
 
-  // TCP sink on the receiver (bob).
-  PacketSinkHelper TCPsink("ns3::TcpSocketFactory",
-                           InetSocketAddress(Ipv4Address::GetAny(), TCP_SINK_PORT));
-  ApplicationContainer TCPSinkApp = TCPsink.Install(nodes.Get(4));
-  TCPSinkApp.Start(Seconds(0.0));
-  TCPSinkApp.Stop(Seconds(MAX_SIMULATION_TIME));
+    // TCP sink on the receiver (bob).
+    PacketSinkHelper TCPsink("ns3::TcpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), TCP_SINK_PORT));
+    ApplicationContainer TCPSinkApp = TCPsink.Install(nodes.Get(4));
+    TCPSinkApp.Start(Seconds(0.0));
+    TCPSinkApp.Stop(Seconds(MAX_SIMULATION_TIME));
 
-  Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
     AsciiTraceHelper ascii;
 
-	// make trace file's name
-	std::string fname2 = "tcp.throughput";
-	Ptr<OutputStreamWrapper> stream2 = ascii.CreateFileStream(fname2);
-	Simulator::Schedule (Seconds (TH_INTERVAL), &TraceThroughput, nodes.Get(4)->GetApplication(1), stream2);
+    // make trace file's name
+    std::string fname2 = "tcp.throughput";
+    Ptr<OutputStreamWrapper> stream2 = ascii.CreateFileStream(fname2);
+    Simulator::Schedule (Seconds (TH_INTERVAL), &TraceThroughput, nodes.Get(4)->GetApplication(1), stream2);
 
-  pp1.EnablePcapAll("PCAPs/tcplow/");
+    pp1.EnablePcapAll("PCAPs/tcplow/");
     Simulator::Stop (Seconds(MAX_SIMULATION_TIME));
-  Simulator::Run();
-  Simulator::Destroy();
-  return 0;
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
 }
