1f13b8ad7ad06f246cbadd5daa6728fdbbb5fa3d
Yuta Takahashi <b1015143@fun.ac.jp>
2019-02-26 13:35:35 +0900

    :tada: Initial commit

---
+++ ns-3.29/build/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/scratch/ns3.29-scratch-simulator-debug', 'build/scratch/subdir/ns3.29-subdir-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
