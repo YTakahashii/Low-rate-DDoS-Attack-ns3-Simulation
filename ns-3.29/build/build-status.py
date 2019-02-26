7071b01225f3c758ca5c734d6ad8e7437144ae28
Yuta Takahashi <b1015143@fun.ac.jp>
2019-02-26 13:44:34 +0900

    :sparkles: Simulate Low-rate DoS Attack
    # ==== Emojis ====
    # 🎉  :tada: Initial commit
    # 🔖  :bookmark: バージョンタグ
    # 🐛  :bug: バグ修正
    # ✏️  :pencil2: タイポ修正
    # 👍  :+1: 機能改善
    # ✨  :sparkles: 部分的な機能追加
    # 💄  :lipstick: UIやスタイルファイルのアップデート
    # 📝  :memo: ドキュメントやコメントの追加
    # ♻️  :recycle: リファクタリング
    # 🗑️  :wastebasket: 不要な機能・使われなくなった機能の削除
    # ✅  :white_check_mark: テスト実装
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # ➕  :heavy_plus_sign: 依存パッケージの追加
    # ➖  :heavy_minus_sign: 依存パッケージの削除
    # 🆙  :up: 依存パッケージのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🚧  :construction: WIP(Work In Progress)
    # 🙈  :see_no_evil: .gitignoreファイルの更新
    # 🏷️  :label: 型の追加やアップデート
    # 🐳  :whale: Dockerの作業

--- ns-3.29/build/build-status.py
+++ ns-3.29/build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.29-scratch-simulator-debug', 'build/scratch/subdir/ns3.29-subdir-debug']
+ns3_runnable_programs = ['build/scratch/ns3.29-scratch-simulator-debug', 'build/scratch/subdir/ns3.29-subdir-debug', 'build/scratch/ns3.29-low-rate-dos-attack-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
